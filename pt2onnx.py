d687b558b5b7d97619d08f1b3f3db8b164f2c09b
xzm <1165428689@qq.com>
2026-08-10 18:20:59 +0800

    ignore root python scripts except train.py

--- pt2onnx.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-import torch
-
-model = YOLO('/mnt/sda1/xzm/Code/ultralytics-main/runs/detect/runs/train/FISHER_DETECT_v11n_p2_1920_0527/weights/best.pt')
-model.export(format='onnx', imgsz=1280, dynamic=True, simplify=True, opset=12, device='cuda')
